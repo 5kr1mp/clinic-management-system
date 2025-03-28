cd0c62f158f95f2e7e79c395a66dc92e69a131fe
5kr1mp <skarlbitang@gmail.com>
2025-03-28 14:53:52 +0800

    Add report functions

--- src/model/Report.java
+++ src/model/Report.java
@@ -11,31 +11,91 @@ public class Report {
     // Variables
     private DateRange dateRange;
     private ArrayList<PatientRecord> patientRecords;
-    private ArrayList<Medicine> medicines;
+    private ArrayList<MedicineBatch> medicineBatches;
 
     // Constructors
-    public Report(DateRange range, ArrayList<PatientRecord> records, ArrayList<Medicine> medicines){
+    public Report(DateRange range, ArrayList<PatientRecord> records, ArrayList<MedicineBatch> medicines){
         this.dateRange = range;
         this.patientRecords = records;
-        this.medicines = medicines;
+        this.medicineBatches = medicines;
     }
 
     // Methods
-    public ArrayList<Medicine> getMedicines() {return medicines;}
+    public ArrayList<MedicineBatch> getMedicineBatches() {return medicineBatches;}
     public ArrayList<PatientRecord> getPatientRecords() {return patientRecords;}
-    public ArrayList<String> getDiagnoses(){
-        ArrayList<String> diagnoses = new ArrayList<>();
-        ArrayList<Object[]> diagnosesCountPair = new ArrayList<>();
 
+    public ArrayList<String> getUniqueDiagnoses(){
+        ArrayList<String> diagnoses = new ArrayList<>();
+
+        // Creates a list of diagnoses from the records.
         for (PatientRecord record : patientRecords) {
-            diagnoses.add(record.getDiagnosis());
+            diagnoses.add(record.getDiagnosis().trim());
         }
 
-        diagnosesCountPair.add(
-            new Object[]{"Fever", Integer.valueOf(10)}
-        );
+        for (String diagnosis : diagnoses) {
+            if (diagnoses.contains(diagnosis)) diagnoses.remove(diagnoses);
+        }
 
         return diagnoses;
+    }
+
+    public ArrayList<String> getAllDiagnoses(){
+        ArrayList<String> diagnoses = new ArrayList<>();
+
+        // Creates a list of diagnoses from the records.
+        for (PatientRecord record : patientRecords) {
+            diagnoses.add(record.getDiagnosis().trim());
+        }
+
+        return diagnoses;
+    }
+
+    public int diagnosisCounter(String diagnosis){
+        int count = 0;
+
+        for (String elem : getAllDiagnoses()) {
+            if (elem.equals(diagnosis)) count++;
+        }
+
+        return count;
+    }
+
+    public ArrayList<Integer> getAllPrescribedMedicinesId(){
+        ArrayList<Integer> prescriptionsId = new ArrayList<>();
+
+        for (PatientRecord record : patientRecords){
+            for (Medicine medicine : record.getMedicineIssued()){
+                prescriptionsId.add(medicine.getId());
+            }
+        }
+
+        return prescriptionsId;
+    }
+
+    public ArrayList<Integer> getUniquePrescribedMedicinesId(){
+        ArrayList<Integer> prescriptionsId = new ArrayList<>();
+
+        for (PatientRecord record : patientRecords){
+            for (Medicine medicine : record.getMedicineIssued()){
+                prescriptionsId.add(medicine.getId());
+            }
+        }
+
+        for (Integer id : prescriptionsId) {
+            if (prescriptionsId.contains(id)) prescriptionsId.remove(id);
+        }
+
+        return prescriptionsId;
+    }
+
+    public int getMedicineIssuedCount(int medicineId){
+        int count = 0;
+
+        for(Integer id : getAllPrescribedMedicinesId()){
+            if (id.equals(medicineId)) count++;
+        }
+
+        return count;
     }
 
     public int getFacultyPatientsCount(){
